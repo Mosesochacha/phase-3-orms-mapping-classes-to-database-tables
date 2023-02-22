8c298d964417d8349ee08d8d167311563a85f5e1
mosesochacha <ochachamoses443@@gmail.com>
2023-02-22 14:33:02 +0300

    done

--- lib/song.rb
+++ lib/song.rb
@@ -35,4 +35,17 @@ class Song
     song = Song.new(name: name, album: album)
     song.save
   end
+   def self.new_from_db(row)
+    self.new(id: row[0], name: row[1] , album: row[2])
+   end
+   
+   def self.all
+    sql =<<-SQL
+    SELECT * FROM songs
+    SQL
+   
+   DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+    end
+  end
 end
